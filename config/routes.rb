44ba6df381757bebceb610447bd1b39eaddd016a
paul lahana <paul.lahana@gmail.com>
2020-03-19 20:05:46 +0100

    plus de texte encore ouais

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home', as: "home"
-  get 'pages/info', as: "info"
+  get 'home', to: "pages#home", as: "home"
+  get 'info',to: "pages#info", as: "info"
 
   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
