6c15c2d3afb2991f663e3206d45f43120c837986
paul lahana <paul.lahana@gmail.com>
2020-04-24 16:46:40 +0200

    coulure all day long

---
+++ app/javascript/packs/coulure.js
@@ -0,0 +1,11 @@
+// document.onload = console.log(document.querySelectorAll(".mixcloud-footer-widget-body-wrapper"));
+
+
+  // const covidBody = document.getElementsByClassName("padd-20 coulure");
+  // console.log(covidBody[0]);
+  // console.log("inside");
+  // const coulure = "<div class='coulure_moving'>ok</div>";
+  // console.log(coulure);
+
+// document.setInterval(covidBody[0].insertAdjacentHTML('afterbegin', coulure), 1000);
+  // console.log(covidBody[0]);
